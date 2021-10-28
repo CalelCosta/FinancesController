350f23f93613af291959114746d21edd6f222b03
Calel Costa <calel.costa@gmail.com>
2021-10-28 16:10:42 -0300

    Adicionando novos métodos e testes para a camada de usuário

--- src/main/java/com/udemyp/myfinances/service/impl/UsuarioServiceImpl.java
+++ src/main/java/com/udemyp/myfinances/service/impl/UsuarioServiceImpl.java
@@ -1,11 +1,15 @@
 package com.udemyp.myfinances.service.impl;
 
+import com.udemyp.myfinances.Exception.ErroAutenticacao;
 import com.udemyp.myfinances.Exception.RegraNegocioException;
 import com.udemyp.myfinances.model.Usuario;
 import com.udemyp.myfinances.repository.UsuarioRepository;
 import com.udemyp.myfinances.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
@@ -15,12 +19,21 @@ public class UsuarioServiceImpl implements UsuarioService {
 
     @Override
     public Usuario autenticar(String email, String senha) {
-        return null;
+        Optional<Usuario> usuario =  usuarioRepository.findByEmail(email);
+        if (!usuario.isPresent()){
+            throw new ErroAutenticacao("Usuário não encontrado para o email informado!");
+        }
+        if(!usuario.get().getSenha().equals(senha)){
+            throw new ErroAutenticacao("Senha Inválida!");
+        }
+        return usuario.get();
     }
 
     @Override
+    @Transactional
     public Usuario salvarUsuario(Usuario usuario) {
-        return null;
+        validarEmail(usuario.getEmail());
+        return usuarioRepository.save(usuario);
     }
 
     @Override
--- src/test/java/com/udemyp/myfinances/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/udemyp/myfinances/repository/UsuarioRepositoryTest.java
@@ -6,23 +6,32 @@ import org.junit.Test;
 
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import java.util.Optional;
+
+
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class UsuarioRepositoryTest {
 
     @Autowired
     UsuarioRepository usuarioRepository;
 
+    @Autowired
+    TestEntityManager entityManager;
+
     @Test
     public void verificaUmEmail(){
         //Cenário
-      Usuario u = Usuario.builder().nome("user").email("user@email.com").build();
-      usuarioRepository.save(u);
+      Usuario u = criarUsuario();
+      entityManager.persist(u);
 
       //Ação
         boolean result = usuarioRepository.existsByEmail("user@email.com");
@@ -33,11 +42,50 @@ public class UsuarioRepositoryTest {
 
     @Test
     public void verificarUsusarioSemEmail(){
-        //Cenário
-        usuarioRepository.deleteAll();
         //Ação
         boolean result = usuarioRepository.existsByEmail("user@email.com");
         //Verificação
         Assertions.assertThat(result).isFalse();
     }
+
+    @Test
+    public void persisteUsuarioNoDB(){
+        //Cenário
+        Usuario u = criarUsuario();
+        //Ação
+        Usuario uSalvo = usuarioRepository.save(u);
+        //Verificação
+        Assertions.assertThat(uSalvo.getId()).isNotNull();
+    }
+
+    @Test
+    public void buscaUsuarioPorEmail(){
+        Usuario u = criarUsuario();
+        entityManager.persist(u);
+
+        Optional<Usuario> result = usuarioRepository.findByEmail("user@email.com");
+
+        Assertions.assertThat(result.isPresent()).isTrue();
+    }
+
+    public static Usuario criarUsuario(){
+        return Usuario
+                .builder()
+                .nome("user")
+                .email("user@email.com")
+                .senha("senha")
+                .build()
+                ;
+    }
+
+    @Test
+    public void retornaVazioSemEmail(){
+        Usuario u = criarUsuario();
+        entityManager.persist(u);
+
+        Optional<Usuario> result = usuarioRepository.findByEmail("");
+
+        Assertions.assertThat(result.isPresent()).isFalse();
+    }
+
 }
