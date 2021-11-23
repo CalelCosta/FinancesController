3008248dcacef1bc658f9a7464679b6afedbf50b
Calel Costa <calel.costa@gmail.com>
2021-11-23 15:19:24 -0300

    Adicionando os métodos de Lançamentos

--- src/main/java/com/udemyp/myfinances/service/LancamentoService.java
+++ src/main/java/com/udemyp/myfinances/service/LancamentoService.java
@@ -1,8 +1,22 @@
 package com.udemyp.myfinances.service;
 
-import com.udemyp.myfinances.repository.LancamentoRepository;
+import com.udemyp.myfinances.model.Lancamento;
+import com.udemyp.myfinances.modelEnums.StatusLancamento;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface LancamentoService {
+    Lancamento salvar(Lancamento lancamento);
+
+    Lancamento atualizar(Lancamento lancamento);
+
+    void deletar(Lancamento lancamento);
+
+    List<Lancamento> buscar(Lancamento lancamentoFiltro);
+
+    void atualizarStatus(Lancamento lancamento, StatusLancamento status);
+
+    void validar(Lancamento lancamento);
 }
